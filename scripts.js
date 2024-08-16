3566c92a4e597ef48391bae826f41fa303d13bf6
Zahra Jafaripoor <zahrajafaripoor1394@gmail.com>
2024-08-16 08:51:45 +0100

    Fixed css to improve button scaling

--- scripts.js
+++ scripts.js
@@ -1,13 +1,15 @@
+// Function to play a sound 
 const playSound = sound => {
     const audio = new Audio(`/sounds/${sound}.mp3`);
     audio.play();
 };
-
+// Function to handle click events on drum key elements
 const handleClick = event => {
     const sound = event.currentTarget.getAttribute('data-sound');
     playSound(sound);
 };
 
+// Function to handle key press events
 const handleKeyPress = event => {
     const keyMap = {
         'q': 'snare',
@@ -19,7 +21,10 @@ const handleKeyPress = event => {
         'u': 'openhat',
         'i': 'tink'
     };
+
+    // sound name based on the pressed key
     const sound = keyMap[event.key.toLowerCase()];
+        // If a valid sound is mapped, play it and simulate a click on the corresponding key element
     if (sound) {
         playSound(sound);
         document.querySelector(`.key[data-sound="${sound}"]`).click();
